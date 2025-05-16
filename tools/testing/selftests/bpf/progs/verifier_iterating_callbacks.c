b58b607e7dad301cc502ba32b511fef1974bfb22
Eduard Zingerman <eddyz87@gmail.com>
2025-05-16 11:35:14 -0700

    selftests/bpf: keep callback function close to iter_limit_bug
    Nothing changes, just more convenient to see all code related to the
    same test grouped together.
    
    Signed-off-by: Eduard Zingerman <eddyz87@gmail.com>

--- tools/testing/selftests/bpf/progs/verifier_iterating_callbacks.c
+++ tools/testing/selftests/bpf/progs/verifier_iterating_callbacks.c
@@ -237,43 +237,6 @@ int bpf_loop_iter_limit_nested(void *unused)
 	return 1000 * a + b + c;
 }
 
-struct iter_limit_bug_ctx {
-	__u64 a;
-	__u64 b;
-	__u64 c;
-};
-
-static __naked void iter_limit_bug_cb(void)
-{
-	/* This is the same as C code below, but written
-	 * in assembly to control which branches are fall-through.
-	 *
-	 *   switch (bpf_get_prandom_u32()) {
-	 *   case 1:  ctx->a = 42; break;
-	 *   case 2:  ctx->b = 42; break;
-	 *   default: ctx->c = 42; break;
-	 *   }
-	 */
-	asm volatile (
-	"r9 = r2;"
-	"call %[bpf_get_prandom_u32];"
-	"r1 = r0;"
-	"r2 = 42;"
-	"r0 = 0;"
-	"if r1 == 0x1 goto 1f;"
-	"if r1 == 0x2 goto 2f;"
-	"*(u64 *)(r9 + 16) = r2;"
-	"exit;"
-	"1: *(u64 *)(r9 + 0) = r2;"
-	"exit;"
-	"2: *(u64 *)(r9 + 8) = r2;"
-	"exit;"
-	:
-	: __imm(bpf_get_prandom_u32)
-	: __clobber_all
-	);
-}
-
 int tmp_var;
 SEC("socket")
 __failure __msg("infinite loop detected at insn 2")
@@ -324,6 +287,43 @@ l0_%=:	.byte 0xe5; /* may_goto */	\
 l1_%=:	r0 = 0;				\
 	exit;				\
 "	::: __clobber_all);
+}
+
+struct iter_limit_bug_ctx {
+	__u64 a;
+	__u64 b;
+	__u64 c;
+};
+
+static __naked void iter_limit_bug_cb(void)
+{
+	/* This is the same as C code below, but written
+	 * in assembly to control which branches are fall-through.
+	 *
+	 *   switch (bpf_get_prandom_u32()) {
+	 *   case 1:  ctx->a = 42; break;
+	 *   case 2:  ctx->b = 42; break;
+	 *   default: ctx->c = 42; break;
+	 *   }
+	 */
+	asm volatile (
+	"r9 = r2;"
+	"call %[bpf_get_prandom_u32];"
+	"r1 = r0;"
+	"r2 = 42;"
+	"r0 = 0;"
+	"if r1 == 0x1 goto 1f;"
+	"if r1 == 0x2 goto 2f;"
+	"*(u64 *)(r9 + 16) = r2;"
+	"exit;"
+	"1: *(u64 *)(r9 + 0) = r2;"
+	"exit;"
+	"2: *(u64 *)(r9 + 8) = r2;"
+	"exit;"
+	:
+	: __imm(bpf_get_prandom_u32)
+	: __clobber_all
+	);
 }
 
 SEC("tc")
