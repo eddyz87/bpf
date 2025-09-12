94646f909b05810e0cb3d91e01865d11f611bba3
Eduard Zingerman <eddyz87@gmail.com>
2025-09-12 03:50:44 -0700

    [debug] bpf: checkpoint logging

--- include/linux/bpf_verifier.h
+++ include/linux/bpf_verifier.h
@@ -831,6 +831,11 @@ struct bpf_verifier_env {
 	/* array of pointers to bpf_scc_info indexed by SCC id */
 	struct bpf_scc_info **scc_info;
 	u32 scc_cnt;
+	struct {
+		s8 spi;
+		s8 reg;
+		s8 frame;
+	} states_equal_log;
 };
 
 static inline struct bpf_func_info_aux *subprog_aux(struct bpf_verifier_env *env, int subprog)
