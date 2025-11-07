b878c3d02bdab0f02ad55fb5a88b1e103ba3092c
Eduard Zingerman <eddyz87@gmail.com>
2025-11-06 16:55:41 -0800

    bpf: immediate dominators computation
    Signed-off-by: Eduard Zingerman <eddyz87@gmail.com>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -19851,6 +19851,10 @@ int bpf_check(struct bpf_prog **prog, union bpf_attr *attr, bpfptr_t uattr,
 	if (ret < 0)
 		goto skip_full_check;
 
+	ret = bpf_compute_idoms(env);
+	if (ret < 0)
+		goto skip_full_check;
+
 	ret = bpf_compute_live_registers(env);
 	if (ret < 0)
 		goto skip_full_check;
@@ -19997,6 +20001,7 @@ err_free_env:
 	kvfree(env->scc_info);
 	kvfree(env->succ);
 	kvfree(env->gotox_tmp_buf);
+	kvfree(env->idoms);
 	kvfree(env);
 	return ret;
 }
