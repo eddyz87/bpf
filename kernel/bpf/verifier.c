2336b88a17f799f1001517df3eda306621ef2b1b
Eduard Zingerman <eddyz87@gmail.com>
2025-11-06 17:36:13 -0800

    bpf: compute loops hierarchy
    Signed-off-by: Eduard Zingerman <eddyz87@gmail.com>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -21440,6 +21440,11 @@ static void clear_insn_aux_data(struct bpf_verifier_env *env, int start, int len
 			aux_data[i].jt = NULL;
 		}
 
+		if (aux_data[i].loop) {
+			kvfree(aux_data[i].loop);
+			aux_data[i].loop = NULL;
+		}
+
 		if (bpf_is_ldimm64(&insns[i]))
 			i++;
 	}
@@ -25171,6 +25176,10 @@ int bpf_check(struct bpf_prog **prog, union bpf_attr *attr, bpfptr_t uattr, __u3
 		goto skip_full_check;
 
 	ret = bpf_compute_idoms(env);
+	if (ret < 0)
+		goto skip_full_check;
+
+	ret = bpf_compute_loops(env);
 	if (ret < 0)
 		goto skip_full_check;
 
